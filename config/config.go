15b3b6744567bd11ca2133688d492d3e9e7da18c
raudhra <rudra888pratap@gmail.com>
2026-03-11 01:01:21 +0530

    set up  confiig file connection to database

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type config struct {
+	DatabaseURL string
+	App_Port    string
+	App_Env     string
+}
+
+func LoadConfig() *config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading the env file")
+	}
+
+	return &config{
+		DatabaseURL: getEnv("DATABASE_URL", "postgres://postgres:2645@localhost:5432/movie-reservation-system?sslmode=disable")
+		App_Port: getEnv("APP_PRT", "8080")
+		App_Env: getEnv("APP_ENV", "development")
+	}
+}
+
+func getEnv(key string, defaultValue string) string{
+	value := os.Getenv(key)
+	if value == ""{
+		return defaultValue
+	}
+	return value
+}
