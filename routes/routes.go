97dea1714bc5f79a1e062f0f7fa09a45b82de57c
raudhra <rudra888pratap@gmail.com>
2026-03-27 06:38:33 +0530

    rewriting routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,25 +1,41 @@
 package routes
 
 import (
-	"log"
-	"net/http"
+	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+	"github.com/raudhra/movie-reservation-system/middleware"
 )
 
-func NewRouter() *mux.Router {
-	r := http.NewServesMux()
-
-	r.HandleFunc("/movies", getAllMovies).Methods("GET")
-	r.HandleFunc("/movies/{ID}", getMovie).Methods("GET")
-	r.HandleFunc("/movie", addMovie).Methods("POST")
-	r.HandleFunc("/movie/{ID}", updateMovie).Methods("PUT")
-	r.HandleFunc("/movie/{ID}", deleteMovie).Methods("DELETE")
-
-	log.Println("Server Starting On Port :8080")
-
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
-		log.Fatal(err)
+func routes() {
+	router := gin.Default()
+	router.POST("/register", register)
+	router.POST("/login", login)
+	signingKey := os.Getenv("JWT_SECRET")
+	//user routes
+	user := router.Group("/user")
+	user.Use(middleware.AuthMiddleware(signingKey))
+	{
+		user.GET("/movies", getAllMovies)
+		user.GET("/movies/:id", getMovie)
+		user.GET("/showtimes", getshowtime)
+		user.GET("/showtimes/:id", getshowtime)
+		user.POST("/reservations", reservation)
 	}
+	admin := router.Group("/admin")
+	admin.Use(middleware.AuthMiddleware(signingKey))
+	{
+		admin.GET("/movies", getAllMovies)
+		admin.GET("/movies/:id", getMovie)
+		admin.GET("/showtimes", getAllShowtimes)
+		admin.GET("/showtimes/:id", getShowtime)
+		admin.POST("/reservations", reservation)
+		admin.POST("/movies", addMovie)
+		admin.POST("/showtimes", addShowtime)
+		admin.PUT("/movies/:id", updateMovie)
+		admin.DELETE("/movies/:id", deleteMovie)
+		admin.PUT("/showtime/:id", updateShowtime)
+		admin.DELETE("/showtime/:id", deleteShowtime)
+	}
+	return *gin.Engine
 }
