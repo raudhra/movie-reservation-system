9bed1c6b4cdd121cc37b67664b61ac45737890a4
raudhra <rudra888pratap@gmail.com>
2026-03-28 17:18:16 +0530

    progressing with auth handlers

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,71 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/raudhra/movie-reservation-system/models"
+)
+
+func getAllMovies(c *gin.Context) {
+	movies := models.GetAllMovies()
+	c.JSON(http.StatusOK, movies)
+}
+
+func getMovie(c *gin.Context) {
+	id := c.Param("id")
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Unable to convert ID to int",
+		})
+		return
+	}
+	movie := models.GetMovie(uint(intId))
+	c.JSON(http.StatusOK, movie)
+}
+
+func addMovie(c *gin.Context) {
+	model := models.Movie{}
+	if err := c.ShouldBindJSON(&model); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Error parsing movie to json",
+		})
+		return
+	}
+	model.AddMovie()
+	c.JSON(http.StatusCreated, model)
+}
+
+func updateMovie(c *gin.Context) {
+	id := c.Param("id")
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Unable to convert ID to int",
+		})
+		return
+	}
+	model := models.Movie{}
+	if err := c.ShouldBindJSON(&model); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Error parsing movie to json",
+		})
+		return
+	}
+	updated := models.UpdateMovie(uint(intId), model)
+	c.JSON(http.StatusOK, updated)
+}
+
+func deleteMovie(c *gin.Context) {
+	id := c.Param("id")
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Unable to convert ID to int",
+		})
+		return
+	}
+	movie := models.DeleteMovie(uint(intId))
+	c.JSON(http.StatusOK, movie)
+}
