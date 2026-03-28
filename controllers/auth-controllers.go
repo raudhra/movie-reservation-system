92e68aaccdfe9b1074c355244b302844d7c588b3
raudhra <rudra888pratap@gmail.com>
2026-03-28 18:41:55 +0530

    set up auth controllers

---
+++ controllers/auth-controllers.go
@@ -0,0 +1,69 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/raudhra/movie-reservation-system/authentication"
+	"github.com/raudhra/movie-reservation-system/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func register(c *gin.Context) {
+	user := models.User{}
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Error while binding User",
+		})
+		return
+	}
+	existing := models.GetUserByEmail(user.Email)
+	if existing.ID != 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "User is already registered",
+		})
+		return
+	}
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Error while hashing password",
+		})
+		return
+	}
+	user.Password = string(hash)
+	user.CreateUser()
+	c.JSON(http.StatusCreated, gin.H{"name": user.Name, "email": user.Email, "role": user.Role})
+
+}
+
+func login(c *gin.Context) {
+	user := models.User{}
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Error While Binding",
+		})
+		return
+	}
+	existing := models.GetUserByEmail(user.Email)
+	if existing.ID == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "User is not registered",
+		})
+		return
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(existing.Password), []byte(user.Password)); err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"error": "Wrong Password",
+		})
+		return
+	}
+	token, err := authentication.GenerateToken(existing.ID, existing.Email, existing.Role)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusConflict, gin.H{
+			"error": "Unable to generate JWT token",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, token)
+
+}
