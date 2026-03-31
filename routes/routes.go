6ee9591266aec35615b7096907141b3503cb0381
raudhra <rudra888pratap@gmail.com>
2026-03-31 12:08:23 +0530

    fixing the routes

--- routes/routes.go
+++ routes/routes.go
@@ -3,40 +3,53 @@ package routes
 import (
 	"os"
 
+	"command-line-arguments/home/raudhra/Documents/Go Lang/Movie Reservation System/controllers/controllers.go"
+	controllers "command-line-arguments/home/raudhra/Documents/Go Lang/Movie Reservation System/controllers/reservation-controllers.go"
+
 	"github.com/gin-gonic/gin"
+	"github.com/raudhra/movie-reservation-system/controllers"
 	"github.com/raudhra/movie-reservation-system/middleware"
 )
 
 func routes() *gin.Engine {
 	router := gin.Default()
-	router.POST("/register", register)
-	router.POST("/login", login)
+	router.POST("/register", controllers.Register)
+	router.POST("/login", controllers.Login)
 	signingKey := os.Getenv("JWT_SECRET")
 	//user routes
 	user := router.Group("/user")
 	user.Use(middleware.AuthMiddleware(signingKey))
 	{
-		user.GET("/movies", getAllMovies)
-		user.GET("/movies/:id", getMovie)
-		user.GET("/showtimes", getAllShowtime)
-		user.GET("/showtimes/:id", getShowtime)
-		user.POST("/reservations", reservation)
+		user.GET("/movies", controllers.GetAllMovies)
+		user.GET("/movies/:id", controllers.GetMovie)
+		user.GET("/showtimes", controllers.GetAllShowtime)
+		user.GET("/showtimes/:id", controllers.GetShowtime)
+		user.POST("/reservations", controllers.CreateReservation)
+		user.GET("/reservations/:id", controllers.GetUserReservations)
+		user.DELETE("/reservations/:id", controllers.CancelReservation)
+		user.GET("/reservations/:id", controllers.)
 	}
 	admin := router.Group("/admin")
 	admin.Use(middleware.AuthMiddleware(signingKey))
-	admin.Use(AdminOnly())
+	admin.Use(middleware.AdminOnly())
 	{
-		admin.GET("/movies", getAllMovies)
-		admin.GET("/movies/:id", getMovie)
-		admin.GET("/showtimes", getAllShowtimes)
-		admin.GET("/showtimes/:id", getShowtime)
-		admin.POST("/reservations", reservation)
-		admin.POST("/movies", addMovie)
-		admin.POST("/showtimes", addShowtime)
-		admin.PUT("/movies/:id", updateMovie)
-		admin.DELETE("/movies/:id", deleteMovie)
-		admin.PUT("/showtime/:id", updateShowtime)
-		admin.DELETE("/showtime/:id", deleteShowtime)
+		admin.GET("/movies", controllers.GetAllMovies)
+		admin.POST("/movies", controllers.AddMovie)
+		admin.GET("/movies/:id", controllers.GetMovie)
+		admin.PUT("/movies/:id", controllers.UpdateMovie)
+		admin.DELETE("/movies/:id", controllers.DeleteMovie)
+		admin.GET("/showtimes", controllers.GetAllShowtimes)
+		admin.GET("/showtimes/:id", controllers.GetShowtime)
+		admin.POST("/showtimes", controllers.AddShowtime)
+		admin.PUT("/showtime/:id", controllers.UpdateShowtime)
+		admin.DELETE("/showtimes/:id", controllers.DeleteShowtime)
+		admin.POST("/reservations", controllers.CreateReservation)
+		admin.GET("/reservations", controllers.GetAllReservation)
+		admin.GET("/reservations/:id", controllers.GetUserReservations)
+		admin.PUT("/reservations/:id", controllers.UpdateReservation)
+		admin.DELETE("/reservations/:id", controllers.CancelReservations)
+
+
 	}
 	return router
 }
