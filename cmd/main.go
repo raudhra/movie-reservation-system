6f016138f827a7ce01b27ad49e54a933f066b1cd
raudhra <rudra888pratap@gmail.com>
2026-03-31 23:06:35 +0530

    setup main func

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"os"
+
+	"github.com/raudhra/movie-reservation-system/config"
+	"github.com/raudhra/movie-reservation-system/routes"
+)
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	config.Connect()
+	router := routes.SetupRoutes()
+	router.Run(":" + port)
+}
