35eb7dd2785251251ff6e97047e8d29095343134
raudhra <rudra888pratap@gmail.com>
2026-04-01 15:45:09 +0530

    debugging

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,12 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/raudhra/movie-reservation-system/config"
 	"github.com/raudhra/movie-reservation-system/routes"
 )
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("APP_PORT")
-	config.Connect()
 	router := routes.SetupRoutes()
 	router.Run(":" + port)
 }
